189d11d73bc0c98e56f30cbc730a5a00a8e0ae12
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-18 22:30:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,10 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif greeting || greeting.upcase
+  elsif greeting != greeting.upcase
     "HUH?! SPEAK UP, SONNY!"
-  else " NO, NOT SINCE 1938!"
+  else
+    " NO, NOT SINCE 1938!"
   end
 end
 
