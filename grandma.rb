88eea3536e4796cb0e223fc9274df2bccea81993
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-18 21:55:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma("I LOVE YOU GRANDMA!")
-end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if speak_to_grandma == "i love you grandma"
-  puts "HUH?! SPEAK UP, SOONY!"
-end
-
-
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
